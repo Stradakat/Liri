b6659663d231e8c59226f3205308ef30048da2ff
Rick Felix <Felirick@gmail.com>
2017-06-21 18:35:07 -0700

    Added Files
    Added Files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '4qRJBbh1kOYs6gGKAGtL7nWeH',
+  consumer_secret: 'HoBwuszuOMFVokkDMcTsY544l4Lx76CfYILBdO3eUmYQXW7ZSi',
+  access_token_key: '32802416-M4ETpdnoTltDB5mybhknWHtf3bYOpNvc0CYncnokT',
+  access_token_secret: 'BrT8EjnyApnsO6bKkDFofKfMTl6KyHukMUOU6KvitoGQT',
+}
